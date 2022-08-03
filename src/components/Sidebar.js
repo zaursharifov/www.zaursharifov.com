036ff95bedf91320e7e25f290018a52a6024265d
Zaur Sharifov <zaursharifov@outlook.com>
2022-08-03 15:21:11 +0300

    sidebar update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,100 @@
+import { useState } from "react";
 import "../style/sidebar.scss";
 export default function Sidebar() {
+  const [isActive] = useState(true);
+
   return (
     <div className="cover">
       <div className="sidebar">
         <span className="line"></span>
-        <h3 className="btn">
-          <span className="num">01</span>Home
-        </h3>
-        <h3 className="btn">
-          <span className="num">02</span>About
-        </h3>
-        <h3 className="btn">
-          <span className="num">03</span>Projects
-        </h3>
-        <h3 className="btn">
-          <span className="num">04</span>Galery
-        </h3>
-        <h3 className="btn">
-          <span className="num">05</span>Contact
-        </h3>
-        <h3 className="btn">
-          <span className="num">06</span>Hire
-        </h3>
+        <div className="btn_container">
+          {isActive ? (
+            <>
+              <span className="num">01</span>
+              <span className="dot">•</span>
+              <a href="/" className="btn">
+                Home
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="/" className="num_reverse">
+                01
+              </a>
+            </>
+          )}
+        </div>
+        <div className="btn_container">
+          {!isActive ? (
+            <>
+              <span className="num">02</span>
+              <span className="dot">•</span>
+              <a href="/" className="btn">
+                Works
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="/" className="num_reverse">
+                02
+              </a>
+            </>
+          )}
+        </div>
+
+        <div className="btn_container">
+          {!isActive ? (
+            <>
+              <span className="num">03</span>
+              <span className="dot">•</span>
+              <a href="/" className="btn">
+                About
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="/" className="num_reverse">
+                03
+              </a>
+            </>
+          )}
+        </div>
+
+        <div className="btn_container">
+          {!isActive ? (
+            <>
+              <span className="num">04</span>
+              <span className="dot">•</span>
+              <a href="/" className="btn">
+                Galery
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="/" className="num_reverse">
+                04
+              </a>
+            </>
+          )}
+        </div>
+
+        <div className="btn_container">
+          {!isActive ? (
+            <>
+              <span className="num">05</span>
+              <span className="dot">•</span>
+              <a href="/" className="btn">
+                Contact
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="/" className="num_reverse">
+                05
+              </a>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
