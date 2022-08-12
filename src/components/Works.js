e0e9fe8ff851c2193a088f6c189c93307d8155dc
Zaur Sharifov <zaursharifov@outlook.com>
2022-08-12 19:33:56 +0300

    slider add to work page

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import images from "../images";
 import "../style/works.css";
-// import { motion } from "framer-motion";
-// import images from "../images";
 
 export default function Works() {
+  const [width, set_width] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    set_width(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <div className="works">
-      {/* <motion.div className="carousel">
-        <motion.div drag="x" className="inner_carousel">
-          {images.map((image) => {
-            return (
-              <motion.div className="work_item">
-                <img src={image} alt="works" />
-              </motion.div>
-            );
-          })}
+      <div className="works_header"></div>
+      <div className="works_slider">
+        <motion.div
+          ref={carousel}
+          className="w_carousel"
+          whileTap={{ cursor: "grabbing" }}
+        >
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            className="w_carousel_inner"
+          >
+            {images.map((item, index) => {
+              return (
+                <motion.div key={index} className="w_item">
+                  <div
+                    className="w_image"
+                    style={{ backgroundImage: `url(${item.i})` }}
+                  />
+                  <div className="w_btns">
+                    <a href={item.l}>Live →</a>
+                    <a href={item.g}>Github →</a>
+                  </div>
+                </motion.div>
+              );
+            })}
+          </motion.div>
         </motion.div>
-      </motion.div> */}
+      </div>
     </div>
   );
 }
--- src/images.js
+++ src/images.js
@@ -1,9 +1,9 @@
-import work1 from "./assets/works/1.jpg";
-import work2 from "./assets/works/2.jpg";
-import work3 from "./assets/works/3.jpg";
-import work4 from "./assets/works/4.jpg";
-import work5 from "./assets/works/5.jpg";
-import work6 from "./assets/works/6.jpg";
-import work7 from "./assets/works/7.jpg";
-const all = [work1, work2, work3, work4, work5, work6, work7];
-export default all;
+import w0 from "./assets/works/old_portfolio_0.png";
+import w1 from "./assets/works/old_portfolio_1.png";
+
+const data = [
+  { i: w0, l: "link0", g: "git0" },
+  { i: w1, l: "link1", g: "git1" },
+];
+
+export default data;
