82a522edb8af1d29e909f02fe31eaea16e57108d
Zaur Sharifov <zaursharifov@outlook.com>
2022-08-15 11:58:48 +0300

    motion carousel deleted from works

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,65 +1,54 @@
-import React, { useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
 import "../style/works.css";
 import data from "../works";
 
 export default function Works() {
   const [arr, set_arr] = useState(data);
   const items = [...new Set(data.map((val) => val.c))];
+
   function filterItem(curcat) {
     const newItem = data.filter((newVal) => {
       return newVal.c === curcat;
     });
     set_arr(newItem);
   }
-  const [width, set_width] = useState(0);
-  const carousel = useRef();
-
-  useEffect(() => {
-    set_width(carousel.current.scrollWidth - carousel.current.offsetWidth);
-  }, [arr]);
   return (
     <div className="works">
       <div className="works_header">
         <button onClick={() => set_arr(data)}>All</button>
         {items.map((val, id) => {
           return (
-            <button key={id} onClick={() => filterItem(val)}>
+            <button
+              key={id}
+              onClick={(e) => {
+                filterItem(val);
+              }}
+            >
               {val}
             </button>
           );
         })}
       </div>
       <div className="works_slider">
-        <motion.div
-          ref={carousel}
-          className="w_carousel"
-          whileTap={{ cursor: "grabbing" }}
-        >
-          <motion.div
-            drag="x"
-            dragConstraints={{ right: 0, left: -width }}
-            className="w_carousel_inner"
-          >
-            {arr.map((item, index) => {
-              return (
-                <motion.div key={index} className="w_item">
-                  <div
-                    onDoubleClick={() => {
-                      window.open(`${item.g}`);
-                    }}
-                    className="w_image"
-                    style={{ backgroundImage: `url(${item.i})` }}
-                  />
-                  <div className="w_btns">
-                    <h2>{item.a}</h2>
-                    <a href={item.l}>→</a>
-                  </div>
-                </motion.div>
-              );
-            })}
-          </motion.div>
-        </motion.div>
+        <div className="slider_inner">
+          {arr.map((item, index) => {
+            return (
+              <div key={index} className="w_item">
+                <div
+                  onDoubleClick={() => {
+                    window.open(`${item.g}`);
+                  }}
+                  className="w_image"
+                  style={{ backgroundImage: `url(${item.i})` }}
+                />
+                <div className="w_btns">
+                  <h2>{item.a}</h2>
+                  <a href={item.l}>→</a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
