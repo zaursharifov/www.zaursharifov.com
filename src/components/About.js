7618e5f276708a067589f5f78e26bb3778369920
Zaur Sharifov <zaursharifov@outlook.com>
2022-08-16 16:48:30 +0300

    shapes add to about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,17 @@ export default function About() {
     <div className="home">
       <div className="top">
         <div>
+          <span className="circle"></span>
+          <span className="triangle"></span>
+          <span className="square"></span>
+          <span className="square2"></span>
           <h1 className="h_h1">Jr. Full Stack Developer</h1>
           <a
             href="./about/cv_zaursharifov.pdf"
             target={"_blank"}
             className="a_btn"
           >
-            RESUME →
+            RESUME <span className="span">→</span>
           </a>
         </div>
         <img className="h_img" src="./about/home.png" alt="home_png" />
