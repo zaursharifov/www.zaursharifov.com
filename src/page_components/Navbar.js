f5e55fa8881b9f80cb99928644bfca07d7b69a92
Zaur Sharifov <zaursharifov@outlook.com>
2022-09-13 18:21:39 +0300

    bug fix

--- src/page_components/Navbar.js
+++ src/page_components/Navbar.js
@@ -7,14 +7,6 @@ export default function Navbar({ isActive, set_isActive }) {
       <a className="logo" href="/">
         ZAUR SHARIFOV
       </a>
-      {/* <button
-        className={`menu_wrapper ${isActive ? "open" : null}`}
-        onClick={() => set_isActive(!isActive)}
-      >
-        <div className="line_menu half start"></div>
-        <div className="line_menu"></div>
-        <div className="line_menu half end"></div>
-      </button> */}
     </div>
   );
 }
