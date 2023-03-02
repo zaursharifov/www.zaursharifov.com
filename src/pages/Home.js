a0c5e736846cae967461fd9b39b71458bec7a2ec
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-02 18:44:22 +0400

    home page implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../style/app.css";
+import "../style/home.css";
 
 export default function Home() {
   return (
     <div className="app-content">
       <Navbar />
-      <div>Home page</div>
+      <div className="top">
+        <div>
+          <h1 className="h_h1">Creative websites for your brand</h1>
+          <button className="h_btn">CONTACT ME →</button>
+        </div>
+        <img className="h_img" src="./about/home.png" alt="home_png" />
+      </div>
+      <div className="h_line"></div>
+      <div className="bottom">
+        <div className="b_section">
+          <h2>Front end development</h2>
+          <p>HTML5 / CSS / Javascript / React JS</p>
+        </div>
+        <div className="b_section">
+          <h2>Back end development</h2>
+          <p>Node js / Mongo DB / MySql</p>
+        </div>
+        <div className="b_section">
+          <h2>Mobile development</h2>
+          <p>React Native</p>
+        </div>
+      </div>
     </div>
   );
 }
