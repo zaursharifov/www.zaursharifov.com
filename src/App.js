3160b2d599494e8e497dd5a95c87fb15448a216a
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-02 15:42:38 +0400

    route delete

--- src/App.js
+++ src/App.js
@@ -1,70 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import React from "react";
 import Sidebar from "./components/Sidebar";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./style/app.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <div className="app-header">
-          <a className="logo" href="/">
-            ZAUR SHARIFOV
-          </a>
-        </div>
-        <Sidebar />
-        <div className="app-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-        <SliderHandle />
+    <div className="app-container">
+      <div className="app-header">
+        <a className="logo" href="/">
+          ZAUR SHARIFOV
+        </a>
       </div>
-    </BrowserRouter>
+      <Sidebar />
+      <div className="app-content">
+        <Home />
+        <Projects />
+        <About />
+        <Contact />
+      </div>
+    </div>
   );
-}
-
-function SliderHandle() {
-  const navigate = useNavigate();
-  const [currentRoute, setCurrentRoute] = useState("/");
-
-  useEffect(() => {
-    const routes = ["/", "/projects", "/about", "/contact"];
-
-    function handleScroll() {
-      const scrollPosition = window.innerHeight + window.scrollY;
-      const maxScrollPosition = document.documentElement.offsetHeight;
-
-      if (scrollPosition >= maxScrollPosition) {
-        const currentIndex = routes.indexOf(currentRoute);
-        const nextIndex = currentIndex === routes.length - 1 ? 0 : currentIndex + 1;
-        const nextRoute = routes[nextIndex];
-        setCurrentRoute(nextRoute);
-        navigate(nextRoute);
-        // window.history.pushState(nextRoute);
-      }
-
-      if (window.scrollY === 0) {
-        const currentIndex = routes.indexOf(currentRoute);
-        const prevIndex = currentIndex === 0 ? routes.length - 1 : currentIndex - 1;
-        const prevRoute = routes[prevIndex];
-        setCurrentRoute(prevRoute);
-        navigate(prevRoute);
-        // window.history.pushState(prevRoute);
-      }
-    }
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [currentRoute, navigate]);
-
-  return null;
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,6 @@
 import React from "react";
+import "../style/app.css";
 
-export default function Sidebar(){
-    return (
-        <div className="sidebar">Sidebar</div>
-    )
+export default function Sidebar() {
+  return <div className="sidebar">Sidebar</div>;
 }
-
-
-
