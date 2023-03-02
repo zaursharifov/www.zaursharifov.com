3e6037b5c7296c1b6689ff7615e8d3b3b495ce26
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-02 19:24:54 +0400

    about done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,30 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import "../style/app.css";
+import "../style/home.css";
 
 export default function About() {
   const visible = true;
   return (
-    <div className={`app-content ${visible && "visible"}`}>
+    <div className={`about ${visible && "visible"}`}>
       <Navbar />
-      <div>About page</div>
+      <div className="a_top">
+        <div>
+          <span className="circle"></span>
+          <span className="triangle"></span>
+          <span className="square"></span>
+          <span className="square2"></span>
+          <h1 className="h_h1">Jr. Full Stack Developer</h1>
+          <a href="./about/cv_zaursharifov.pdf" target={"_blank"} className="a_btn">
+            RESUME <span className="span">→</span>
+          </a>
+        </div>
+        <div className="a_bottom">
+          <img className="a_img" src="./about/companies/bitsody.jpg" alt="bitsody" />
+          <img className="a_img" src="./about/companies/buta.jpg" alt="buta" />
+          <img className="a_img" src="./about/companies/fiverr.jpg" alt="fiverr" />
+          <img className="a_img" src="./about/companies/utest.png" alt="utest" />
+        </div>
+      </div>
     </div>
   );
 }
