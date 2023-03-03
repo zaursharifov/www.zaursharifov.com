e819df1cee09e722bf84f6b52e7ef88df372f06d
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-03 15:12:04 +0400

    contact change start

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,71 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import "../style/app.css";
+import "../style/contact.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Contact() {
   const visible = true;
   return (
-    <div className={`app-content ${visible && "visible"}`}>
-      <Navbar />
-      <div>Contact page</div>
-    </div>
+    <>
+      <img className="pin" src="./about/maps/circle.png" alt="pin" />
+      <div className={`contact ${visible && "visible"}`}/>
+      <div className="card">
+        <div className="c_txt_container">
+          <button className="c_txt">
+            <a
+              className="c_txt"
+              href="mailto:zaur.sharifov@outlook.com"
+              rel="noreferrer"
+            >
+              zaur.sharifov@outlook.com
+            </a>
+          </button>
+          <button
+            className="c_txt"
+            onClick={() => {
+              window.open("https://wa.me/+994705741452");
+            }}
+          >
+            +994 70 574 14 52
+          </button>
+          {/* <button
+            className="c_txt"
+            onClick={() => {
+              window.open("https://wa.me/+905536441452");
+            }}
+          >
+            +905 53 644 14 52
+          </button> */}
+        </div>
+
+        <div className="c_btn_container">
+          <button
+            className="c_btn"
+            onClick={() => {
+              window.open("https://www.instagram.com/zeraphosa/");
+            }}
+          >
+            <InstagramIcon />
+          </button>
+          <button
+            className="c_btn"
+            onClick={() => {
+              window.open("https://www.linkedin.com/in/zaursharifov-404/");
+            }}
+          >
+            <LinkedInIcon />
+          </button>
+          <button
+            className="c_btn"
+            onClick={() => {
+              window.open("https://github.com/zeraphosa");
+            }}
+          >
+            <GitHubIcon />
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
