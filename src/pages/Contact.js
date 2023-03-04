bfb6300f030a549bf68c4201199fd1359bc71279
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-04 15:12:45 +0400

    about links done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,21 +8,11 @@ export default function Contact() {
     <>
       <Header />
       <div className={`contact ${visible && "visible"}`}>
-        <div className="img-border-container">
-          <div className="border edge-l"></div>
-          <div className="border edge-r"></div>
-          <div className="border edge-t"></div>
-          <div className="border edge-b"></div>
-          <div className="border corner-tl"></div>
-          <div className="border corner-tr"></div>
-          <div className="border corner-bl"></div>
-          <div className="border corner-br"></div>
-          <img className="pin border-content" src="./about/maps/circle.png" alt="pin" />
-        </div>
+        <img className="pin border-content" src="./about/maps/circle.png" alt="pin" />
         <form className="card" id="mail">
-            <input type="text" placeholder="name"/>
-            <textarea form="mail" placeholder="message"/>
-            <input type="submit" placeholder="Submit"/>
+          <input type="text" placeholder="name" />
+          <textarea form="mail" placeholder="message" />
+          <input type="submit" placeholder="Submit" />
         </form>
       </div>
     </>
