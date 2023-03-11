8b80cad717701e3449b2ae528e9e8595238becb3
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-11 23:07:00 +0400

    scroll stop onfocus

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Header from "../components/Header";
 import "../style/contact.css";
 
-export default function Contact({focusInput, setFocusInput }) {
+export default function Contact({ focusInput, setFocusInput }) {
   const visible = true;
+  const inputRef = useRef();
+
+  useEffect(()=>{
+    function handleFocus(){
+      setFocusInput(true);
+    }
+
+    inputRef.current.addEventListener('onfocus',handleFocus);
+    return()=>{
+      inputRef.current.removeEventListener('onfocus',handleFocus);
+    }
+  },[])
+
   return (
     <>
       <Header />
@@ -12,7 +25,7 @@ export default function Contact({focusInput, setFocusInput }) {
           <img className="pin" src="./about/maps/circle.png" alt="pin" />
         </div>
         <div className="card" id="mail">
-          <input type="text" placeholder="name" onFocus={() => setFocusInput(!focusInput)}/>
+          <input type="text" placeholder="name" ref={inputRef} />
           <span class="textarea" role="textbox" contenteditable="true"></span>
           <div>
             <button type="submit" className="a_btn">
