12bdd8bf63f742c3cb516332325d7b4cf7a24dad
zeraphosa <zaursharifov@outlook.com>
2023-07-06 18:25:30 +0400

    graphql query add

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,30 @@
 import { request, gql } from "graphql-request";
 
 export const getProjects = async () => {
-    const query = gql`
-        query MyQuery{
-            
+  const query = gql`
+    query Assets {
+      projectsConnection {
+        edges {
+          node {
+            github
+            id
+            live
+            name
+            url
+            description
+          }
         }
-    `
-}
+      }
+      bookmarksConnection {
+        edges {
+          node {
+            desc
+            id
+            link
+            name
+          }
+        }
+      }
+    }
+  `;
+};
