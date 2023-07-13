a3cee78b711ab0cebf04cd6ae362ccc8467ff09f
zeraphosa <zaursharifov@outlook.com>
2023-07-13 19:47:48 +0400

    .env.local

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ["media.graphassets.com"],
   },
   env: {
-    GRAPHQL_CMS_API: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cljr5v0nv09y901tc4sb456vd/master',
+    GRAPHQL_CMS_API: process.env.GRAPHQL_CMS_API,
   },
 };
 
